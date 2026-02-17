3f5f55379a8b8c35ce3aa9f1250c875796b5b6e6
Kevin Pors <krpors@gmail.com>
2026-02-17 23:11:51 +0100

    screwing about with states

--- main.go
+++ main.go
@@ -1,30 +1,39 @@
 package main
 
 import (
-	"image/color"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/vector"
 )
 
+// State defines the interface for a game state.
+type State interface {
+	Update() error
+	Draw(screen *ebiten.Image)
+}
+
 type Game struct {
-	Radians float32
+	currentState State
+}
+
+func NewGame() *Game {
+	game := &Game{}
+	game.currentState = &StateTest{}
+	return game
 }
 
 func (g *Game) Update() error {
-	g.Radians += 1 / float32(ebiten.TPS())
+	if g.currentState != nil {
+		return g.currentState.Update()
+	}
+
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	img := ebiten.NewImage(320, 240)
-	vector.FillRect(img, 0, 0, 80, 80, color.RGBA{0xff, 0xff, 0xcc, 0x22}, true)
-	op := &ebiten.DrawImageOptions{}
-	op.GeoM.Translate(-40, -40)
-	op.GeoM.Rotate(float64(g.Radians))
-	op.GeoM.Translate(40, 40)
-	screen.DrawImage(img, op)
+	if g.currentState != nil {
+		g.currentState.Draw(screen)
+	}
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -34,7 +43,10 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 func main() {
 	ebiten.SetWindowSize(640, 480)
 	ebiten.SetWindowTitle("Hello, World!")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+
+	game := NewGame()
+
+	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
 	}
 }
---
+++ statetest.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"image/color"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/vector"
+)
+
+type StateTest struct {
+	Radians float32
+}
+
+func (s *StateTest) Update() error {
+	s.Radians += 0.5 / float32(ebiten.TPS())
+	return nil
+}
+
+func (s *StateTest) Draw(screen *ebiten.Image) {
+	img := ebiten.NewImage(320, 240)
+	vector.FillRect(img, 0, 0, 80, 80, color.RGBA{0xff, 0xff, 0xcc, 0x22}, true)
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(-40, -40)
+	op.GeoM.Rotate(float64(s.Radians))
+	op.GeoM.Translate(40, 40)
+	screen.DrawImage(img, op)
+
+	tri := ebiten.NewImage(320, 240)
+	path := vector.Path{}
+	path.LineTo(0, 0)
+	path.LineTo(40, 0)
+	path.LineTo(20, 60)
+	vector.FillPath(tri, &path, &vector.FillOptions{}, &vector.DrawPathOptions{})
+
+	op = &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(-float64(path.Bounds().Dx())/2, -float64(path.Bounds().Dy())/2)
+	op.GeoM.Rotate(float64(s.Radians))
+	op.GeoM.Translate(90, 90)
+	op.ColorScale.SetR(1.0)
+	op.ColorScale.SetG(0)
+	op.ColorScale.SetB(0)
+
+	screen.DrawImage(tri, op)
+}
